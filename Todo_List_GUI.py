337727fcdc845339cb22963a0d09d021344e2321
Jaisharan <jaisharan3007@gmail.com>
2023-10-22 12:51:51 -0400

    added edit functionality (match case) and constant updates to the program

--- Todo_List_GUI.py
+++ Todo_List_GUI.py
@@ -5,14 +5,42 @@ label = sg.Text("Type in a todo")
 input = sg.InputText(tooltip="Enter a todo", key="todo")
 add = sg.Button("Add")
 
-window = sg.Window("Todo List", layout=[[label],[input, add], []], font=("Times New Roman", 15))
+todos = functions.get_todos()
+for todo in todos:
+    index = todos.index(todo)
+    todos[index] = todo.strip("\n")
+
+
+list_box = sg.Listbox(values=todos, key="show_todos", enable_events=True, size=[45,10])
+edit_button = sg.Button("Edit")
+
+window = sg.Window("Todo List", layout=[[label, input, add],[list_box, edit_button]], font=("Times New Roman", 15))
+
+edit = ""
+
 while True:
     event, value = window.read()
-    if event == "Add":
-        todos = functions.get_todos()
-        todos.append(value["todo"] + "\n")
-        functions.write_todos(todos)
-    if event == sg.WINDOW_CLOSED:
-        break
+    print(1, event)
+    print(2, value)
+    print(3, value["show_todos"])
+    match event:
+        case "Add":
+            todos = functions.get_todos()
+            todos.append(value["todo"] + "\n")
+            functions.write_todos(todos)
+            window["show_todos"].update(values=todos)
 
+        case sg.WINDOW_CLOSED:
+            break
+
+        case "Edit":
+            todo_edit = value["show_todos"][0]
+            new_todo = value["todo"] + "\n"
+
+            todos = functions.get_todos()
+            index = todos.index(todo_edit)
+            todos[index] = new_todo
+
+            functions.write_todos(todos)
+            window["show_todos"].update(values=todos)
 window.close()
