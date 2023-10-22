68a881f724cb39f64911bd3871d3e7b2c7e58bc5
Jaisharan <jaisharan3007@gmail.com>
2023-10-22 11:31:28 -0400

    added "Add Todo" functionality

--- Todo_List_GUI.py
+++ Todo_List_GUI.py
@@ -2,9 +2,17 @@ import functions
 import PySimpleGUI as sg
 
 label = sg.Text("Type in a todo")
-input = sg.InputText(tooltip="Enter a todo")
+input = sg.InputText(tooltip="Enter a todo", key="todo")
 add = sg.Button("Add")
 
-window = sg.Window("Todo List", layout=[[label],[input, add], []])
-window.read()
+window = sg.Window("Todo List", layout=[[label],[input, add], []], font=("Times New Roman", 15))
+while True:
+    event, value = window.read()
+    if event == "Add":
+        todos = functions.get_todos()
+        todos.append(value["todo"] + "\n")
+        functions.write_todos(todos)
+    if event == sg.WINDOW_CLOSED:
+        break
+
 window.close()
